e0344df3bcd5b64c731be6eac2fc457d27c85195
Zahin Azmayeen <zahin06@gmail.com>
2026-06-23 12:56:55 +0600

    add missing files for build

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # This pins requirements.txt provided by zephyr-nix.pythonEnv.
+    zephyr.url = "github:zmkfirmware/zephyr/v4.1.0+zmk-fixes";
+    zephyr.flake = false;
+
+    # Zephyr sdk and toolchain.
+    zephyr-nix.url = "github:nix-community/zephyr-nix";
+    zephyr-nix.inputs.zephyr.follows = "zephyr";
+    zephyr-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, zephyr-nix, ... }: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        zephyr = zephyr-nix.packages.${system};
+      in {
+        default = pkgs.mkShellNoCC {
+          packages =
+            [
+              zephyr.pythonEnv
+              (zephyr.sdk-0_16.override {targets = ["arm-zephyr-eabi"];})
+
+              pkgs.cmake
+              pkgs.dtc
+              pkgs.gcc
+              pkgs.ninja
+
+              pkgs.just
+              pkgs.yq # Make sure yq resolves to python-yq.
+            ];
+
+          env = {
+            PYTHONPATH = "${zephyr.pythonEnv}/${zephyr.pythonEnv.sitePackages}";
+          };
+
+          shellHook = ''
+            export ZMK_BUILD_DIR=$(pwd)/.build;
+            export ZMK_SRC_DIR=$(pwd)/zmk/app;
+          '' + (if pkgs.stdenv.isLinux then
+            let libatomic = pkgs.runCommand "libatomic" {} ''
+              mkdir -p $out/lib
+              cp -d ${pkgs.stdenv.cc.cc.lib}/lib/libatomic.so* $out/lib/
+            ''; in ''
+            export LD_LIBRARY_PATH="${libatomic}/lib";
+          '' else "");
+        };
+      }
+    );
+  };
+}
